28336720bfdfb3bed8428b6e5f6edd8f94e49223
leosoplapuco <leosoplapuco29@gmail.com>
2024-03-19 10:45:01 -0500

    Upgrade: reac_intro folder

---
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+import closeIcon from '../assets/logo-holberton.png';
+
+
+export default function Notifications() {
+	return (
+			<div className="Notifications">
+			<button style={{ background: 'none', border: 'none', position: 'absolute', right: '15px', top: '20px', cursor: 'pointer', }} aria-label="Close" onClick={(e) => { console.log('Close button has been clicked'); }}>
+				<img src={closeIcon} alt="closeicon" style={{ width: '25px', height: '25px', }} />
+			</button>
+			<p style={{ fontSize:'20px',}}>Here is the list of notifications</p>
+			<ul>
+				<li data-priority="default">New course available</li>
+				<li data-priority="urgent">New resume available</li>
+				<li data-priority="urgent" dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
+			</ul>
+		</div>
+	);
+};
---
+++ react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App.js';
+import Notifications from './Notifications/Notifications';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Notifications />
+        <App />
+    </React.StrictMode>
+);
