86537b065c00916b75d09b905b132d6f3eb58580
leosoplapuco <leosoplapuco29@gmail.com>
2024-03-28 10:39:04 -0500

    Mod: App.test.js

--- React_component/task_1/dashboard/src/App/App.test.js
+++ React_component/task_1/dashboard/src/App/App.test.js
@@ -1,33 +1,51 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
-import Notifications from './Notifications';
-import Header from './Header';
-import Login from './Login';
-import Footer from './Footer';
+import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
 
-describe('App component', () => {
-    it('should render without crashing', () => {
-        shallow(<App />);
+describe('App Component', () => {
+    it('renders without crashing', () => {
+        const wrapper = shallow(<App />);
+        const appComponent = wrapper.find('.App');
+        expect(appComponent.length).toBe(1);
     });
 
-    it('should contain the Notifications component', () => {
+    it('contains the Notifications component', () => {
         const wrapper = shallow(<App />);
-        expect(wrapper.find(Notifications)).toHaveLength(1);
+        expect(wrapper.find(Notifications).length).toBe(1);
     });
 
-    it('should contain the Header component', () => {
+    it('contains the Header component', () => {
         const wrapper = shallow(<App />);
-        expect(wrapper.find(Header)).toHaveLength(1);
+        expect(wrapper.find(Header).length).toBe(1);
     });
 
-    it('should contain the Login component', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(Login)).toHaveLength(1);
+    it('contains the Login component when isLoggedIn is false', () => {
+        const wrapper = shallow(<App isLoggedIn={false} />);
+        expect(wrapper.find(Login).length).toBe(1);
     });
 
-    it('should contain the Footer component', () => {
+    it('does not contain the CourseList component when isLoggedIn is false', () => {
+        const wrapper = shallow(<App isLoggedIn={false} />);
+        expect(wrapper.find(CourseList).length).toBe(0);
+    });
+
+    it('does not contain the Login component when isLoggedIn is true', () => {
+        const wrapper = shallow(<App isLoggedIn={true} />);
+        expect(wrapper.find(Login).length).toBe(0);
+    });
+
+    it('contains the CourseList component when isLoggedIn is true', () => {
+        const wrapper = shallow(<App isLoggedIn={true} />);
+        expect(wrapper.find(CourseList).length).toBe(1);
+    });
+
+    it('contains the Footer component', () => {
         const wrapper = shallow(<App />);
-        expect(wrapper.find(Footer)).toHaveLength(1);
+        expect(wrapper.find(Footer).length).toBe(1);
     });
 });
