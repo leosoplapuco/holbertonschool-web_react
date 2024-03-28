c933d1ae999f7f0833c747bf17cb29fbc5afea38
leosoplapuco <leosoplapuco29@gmail.com>
2024-03-28 15:03:43 -0500

    React_inline_styling: Task_1 06

--- React_inline_styling/task_1/dashboard/src/Header/Header.test.js
+++ React_inline_styling/task_1/dashboard/src/Header/Header.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Header from './Header';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+});
 
 describe('Header component', () => {
     it('should render without crashing', () => {
